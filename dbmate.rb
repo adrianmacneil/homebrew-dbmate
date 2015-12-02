0bc58fabfcf6f5c615ed768fb4450642a34915c1
Adrian Macneil <adrian@adrianmacneil.com>
2015-12-01 19:25:22 -0800

    Add formula

---
+++ dbmate.rb
@@ -0,0 +1,24 @@
+class Dbmate < Formula
+  desc "A lightweight, framework-agnostic database migration tool."
+  homepage "https://github.com/adrianmacneil/dbmate"
+  url "https://github.com/adrianmacneil/dbmate/archive/v0.1.tar.gz"
+  version "0.1"
+  sha256 "bba83b4feee69ac5a93c4d64d4f44d9361240cbcfc13ef3c27ceb81ef7860c11"
+
+  head "https://github.com/adrianmacneil/dbmate.git"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+
+    system "go", "get", "-d", "-v"
+    system "go", "build", "-o", "dbmate"
+
+    bin.install "dbmate"
+  end
+
+  test do
+    system "#{bin}/dbmate", "-v"
+  end
+end
