a8078aafed07f4c51737c660ff6455ed699c65a6
Adrian Macneil <adrian@adrianmacneil.com>
2015-12-03 09:36:33 -0800

    Appease brew audit

--- dbmate.rb
+++ dbmate.rb
@@ -1,8 +1,7 @@
 class Dbmate < Formula
-  desc "A lightweight, framework-agnostic database migration tool."
+  desc "Lightweight, framework-agnostic database migration tool."
   homepage "https://github.com/amacneil/dbmate"
   url "https://github.com/amacneil/dbmate/archive/v0.1.tar.gz"
-  version "0.1"
   sha256 "bba83b4feee69ac5a93c4d64d4f44d9361240cbcfc13ef3c27ceb81ef7860c11"
 
   head "https://github.com/amacneil/dbmate.git"
