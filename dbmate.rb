2eda49956f98d6c0efc70b33ce101f5813c7d55d
Adrian Macneil <adrian@getcruise.com>
2016-08-07 23:48:06 -0700

    v1.1.0

--- dbmate.rb
+++ dbmate.rb
@@ -1,15 +1,15 @@
 class Dbmate < Formula
   desc "Lightweight, framework-agnostic database migration tool."
   homepage "https://github.com/amacneil/dbmate"
-  url "https://github.com/amacneil/dbmate/archive/v1.0.1.tar.gz"
-  sha256 "1eb41d3d3f1b7d273ec199dcac5cc5d023ac5d87481faad7c652b63bc9d12aed"
+  url "https://github.com/amacneil/dbmate/archive/v1.1.0.tar.gz"
+  sha256 "a7d57964da180ed54ea307c0dee38a8009f5df9f03689ec7ea0dbea8e8b62d99"
 
   head "https://github.com/amacneil/dbmate.git"
 
   bottle do
     root_url "https://github.com/amacneil/dbmate/releases/download/v1.0.1"
     cellar :any
-    sha256 "db0960070131ca18bc0488e8be126861bcfb167df8653ee4a191b3c17c78579f" => :el_capitan
+    sha256 "e2158716ab2b9968e0317b941aef40d5ba493dd02784de550148ce3525f18fe3" => :el_capitan
   end
 
   depends_on "go" => :build
