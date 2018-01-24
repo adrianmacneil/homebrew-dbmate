e718943c065876f18a695d9c7a1aafa06939ecbd
Adrian Macneil <adrian@getcruise.com>
2018-01-23 18:08:38 -0800

    v1.3.0

--- dbmate.rb
+++ dbmate.rb
@@ -1,25 +1,27 @@
 class Dbmate < Formula
   desc "Lightweight, framework-agnostic database migration tool."
   homepage "https://github.com/amacneil/dbmate"
-  url "https://github.com/amacneil/dbmate/archive/v1.2.1.tar.gz"
-  sha256 "046b20ff292dd80e5c9832ade01f67fb3631cccc2eeb372de51f32d375b16377"
+  url "https://github.com/amacneil/dbmate/archive/v1.3.0.tar.gz"
+  sha256 "f449fd1529067f9dd8fc9993b51b450c14ea61ed628d7256f7c58bcaed77bf60"
 
   head "https://github.com/amacneil/dbmate.git"
 
   bottle do
-    root_url "https://github.com/amacneil/dbmate/releases/download/v1.2.1"
+    root_url "https://github.com/amacneil/dbmate/releases/download/v1.3.0"
+    sha256 "11b21beb647193756b6027f946c124cad231eac50dc247f9bdf06ec66724582c" => :high_sierra
     cellar :any
-    sha256 "b0757628001e0002548320dc8aac128710bef3becc04f94c583ed5afe71a4c97" => :el_capitan
   end
 
   depends_on "go" => :build
+  depends_on "dep" => :build
 
   def install
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/amacneil/dbmate").install buildpath.children
 
     cd "src/github.com/amacneil/dbmate" do
-      system "go", "build", "-o", bin/"dbmate"
+      system "dep", "ensure", "-vendor-only"
+      system "go", "build", "-ldflags", "-s", "-o", bin/"dbmate", "./cmd/dbmate"
     end
   end
 
